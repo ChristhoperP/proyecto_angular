4ad19295a5f1cd693a9269f9ec4b3ef9e4dbea82
yohesler@gmail.com <43556496+HeslerG9@users.noreply.github.com>
2020-09-02 15:32:21 -0600

    Probando Template Admin
    gg

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -9,6 +9,7 @@ import { ContactoComponent } from "./contacto/contacto.component";
 import { RegistrarseComponent } from "./registrarse/registrarse.component";
 import { IniciaSesionComponent } from "./inicia-sesion/inicia-sesion.component";
 import { LogoutComponent } from "./logout/logout.component";
+import { AdminComponent } from "./admin/admin.component";
 
 import { AuthGuard } from "./guards/auth.guard";
 
@@ -18,6 +19,7 @@ const appRoutes: Routes = [
     {path: 'inicio', component: InicioComponent},
     {path: 'amigos', component: AmigosComponent, canActivate: [AuthGuard]},
     {path: 'contacto', component: ContactoComponent, canActivate: [AuthGuard]},
+    {path: 'admin', component: AdminComponent, canActivate: [AuthGuard]},
     {path: 'registrarse', component: RegistrarseComponent},
     {path: 'iniciar-sesion', component: IniciaSesionComponent},
     {path: 'logout', component: LogoutComponent},
