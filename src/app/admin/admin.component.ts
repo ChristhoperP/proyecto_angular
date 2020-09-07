5f229041c9af62881ba27df3d06831800ace253f
yohesler@gmail.com <43556496+HeslerG9@users.noreply.github.com>
2020-09-06 20:21:09 -0600

    Pruebas de Admin #1
    Pruebas de Admin #1

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from "../services/user.service";
+import { AuthService } from "../services/auth.service";
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+  public user: any;
 
-  constructor() { }
+  constructor(private _userService: UserService, private _authService: AuthService) {
+    this.user={
+      nombre: "",
+      apellidos: "",
+      username:"",
+      email: ""
+    };}
 
   ngOnInit(): void {
+    this._userService.getUser().subscribe(
+      response=>{
+        console.log(response);
+        this.user.nombre=response.nombre;
+        this.user.apellidos=response.apellidos;
+        this.user.username=response.username;
+        this.user.email=response.email;
+      },
+      error=>{
+        alert(error.error.message);
+        this._authService.logoutUser();
+      }
+    );
   }
 
 }
