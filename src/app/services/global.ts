70e1ccdd665a17b5b644006cf4ffbf5f8e932b7f
ChristhoperP <43559198+ChristhoperP@users.noreply.github.com>
2020-09-17 21:40:03 -0600

    nueva direccion de api rest

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,6 +1,7 @@
 export var Global = {
     //Production
-    url: "https://gatitos-app.herokuapp.com/api/"
+    //url: "https://gatitos-app.herokuapp.com/api/"
+    url: "http://cactus-289500.appspot.com/api/"
     //Dev
     //url: "http://localhost:3700/api/"
 }
